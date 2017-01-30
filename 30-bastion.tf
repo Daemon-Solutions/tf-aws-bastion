46e4faab05928887c2be58627bb96ab4addf7aa0
Liam Bennett <liam@bashton.com>
2017-01-30 13:24:22 +0000

    lists

--- 30-bastion.tf
+++ 30-bastion.tf
@@ -6,14 +6,14 @@ module "bastion" {
   envname = "${var.envname}"
   service = "bastion"
 
-  availability_zones = "${lookup(var.aws_zones, var.aws_region)}"
-  subnets            = "${join(",",var.public_subnets)}"
+  availability_zones = ["${lookup(var.aws_zones, var.aws_region)}"]
+  subnets            = ["${var.public_subnets}"]
 
   key_name                    = "bashton"
   ami_id                      = "${var.bastion_ami}"
   instance_type               = "t2.micro"
   iam_instance_profile        = "${module.iam_profile_bastion.profile_id}"
-  security_groups             = "${aws_security_group.bastion_sg.id},${aws_security_group.bastion_egress.id}"
+  security_groups             = ["${aws_security_group.bastion_sg.id}","${aws_security_group.bastion_egress.id}"]
   user_data                   = "${var.bastion_userdata}${data.template_file.bastion_userdata.rendered}"
   associate_public_ip_address = true
 
