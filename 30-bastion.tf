e1d46038d25ef6427d118d089e714a519639354b
Steve Smith <steve@bashton.com>
2017-02-28 11:21:29 +0000

    Make module compatible with 'tf-aws-asg' module v0.2.0

--- 30-bastion.tf
+++ 30-bastion.tf
@@ -1,4 +1,3 @@
-
 module "bastion" {
   source = "../tf-aws-asg"
 
@@ -6,17 +5,16 @@ module "bastion" {
   envname = "${var.envname}"
   service = "bastion"
 
-  availability_zones = ["${split(",",lookup(var.aws_zones, var.aws_region))}"]
-  subnets            = ["${var.public_subnets}"]
+  subnets = ["${var.public_subnets}"]
 
   key_name                    = "bashton"
   ami_id                      = "${var.bastion_ami}"
   instance_type               = "t2.micro"
   iam_instance_profile        = "${module.iam_profile_bastion.profile_id}"
-  security_groups             = ["${aws_security_group.bastion_sg.id}","${aws_security_group.bastion_egress.id}"]
+  security_groups             = ["${aws_security_group.bastion_sg.id}", "${aws_security_group.bastion_egress.id}"]
   user_data                   = "${var.bastion_userdata}${data.template_file.bastion_userdata.rendered}"
   associate_public_ip_address = true
 
-  asg_min = "${var.bastion_asg_min}"
-  asg_max = "${var.bastion_asg_max}"
+  min = "${var.bastion_asg_min}"
+  max = "${var.bastion_asg_max}"
 }
